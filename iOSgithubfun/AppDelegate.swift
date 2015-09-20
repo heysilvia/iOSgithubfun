034cf83b21b8688f0c88341f62c08d9d98049b7c
Silvia Bormüller <sbormueller@gmail.com>
2015-09-20 23:54:49 +0200

    added a comment

--- iOSgithubfun/AppDelegate.swift
+++ iOSgithubfun/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    //added a hello world comment
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
